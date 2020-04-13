dc18448b5cb363d1327faba41df621fa5e5ede02
Daniel Laloush <d.laloush@outlook.com>
2020-04-13 18:57:43 -0400

    Adde basic structure, board and click function

---
+++ README.md
@@ -0,0 +1,65 @@
+# JS-Library
+[![License](https://img.shields.io/badge/License-MIT-green.svg)]() [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/Dandush03/capstone-build-linter) 
+
+
+#### Microverse First JS Project
+
+![Screenshot from 2020-04-10 19-05-06](https://user-images.githubusercontent.com/51087936/79028673-012c2000-7b5f-11ea-94f5-b46fb0516a26.png)
+
+---
+
+## Table of Contents
+
+- [Built With](#Built-With)
+- [Live Link](#Active-Link-to-Website)
+- [Contributing](#Contributing)
+- [Acknowledgments](#Acknowledgments)
+- [License](#License)
+- [Author](#Authors)
+
+---
+
+## Built With
+
+- JavaScript
+- GitHub
+- Vim
+- VSCode
+
+---
+
+## Active Link to Website
+
+Check the page in this [Live Link](https://raw.githack.com/Dandush03/JS-Library/feature/setup/index.html)
+
+---
+
+## Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](./issues/).
+
+---
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Thanks to Microverse for it Support!
+
+---
+
+## License
+
+- **[MIT license](http://opensource.org/licenses/mit-license.php)**
+
+## Author
+
+**Danie Laloush**
+
+- Portafolio: [dlaloush.me](https://dlaloush.me)
+- Linkedin: [Daniel Laloush](https://www.linkedin.com/in/daniel-laloush-0a7331a9)
+- Github: [@Dandush03](https://github.com/Dandush03)
+- Twitter: [@d_laloush](https://twitter.com/d_laloush)
+
+Give a ⭐️ if you like this project!
---
+++ script.js
@@ -0,0 +1,60 @@
+const Player = (name, input) => ({ name, input });
+
+const Game = (p1, p2) => ({ p1, p2 });
+let game = '';
+let turn = '';
+
+function clicked() {
+  let row = this.parentNode.className;
+  row = row.split('-');
+  row = row.pop();
+  let clm = this.attributes.name.nodeValue;
+  clm = clm.split('-');
+  clm = clm.pop();
+  console.log(clm);
+  console.log(row);
+  if (turn.input === 'x') {
+    this.innerHTML = '<i class="fas fa-times"></i>';
+    if (turn === game.p1) {
+      turn = game.p2;
+    } else {
+      turn = game.p1;
+    }
+  } else {
+    this.innerHTML = '<i class="far fa-circle"></i>';
+    if (turn === game.p1) {
+      turn = game.p2;
+    } else {
+      turn = game.p1;
+    }
+  }
+}
+
+const Board = () => {
+  const array = [[1, 2, 3], [1, 2, 3], [1, 2, 3]];
+  const show = () => array.forEach((objs) => {
+    const div = document.createElement('div');
+    div.setAttribute('class', `row-${array.indexOf(objs)}`);
+    div.setAttribute('name', `row-${array.indexOf(objs)}`);
+    objs.forEach((obj) => {
+      const span = document.createElement('span');
+      span.setAttribute('name', `clmn-${objs.indexOf(obj)}`);
+      span.innerHTML = '';
+      div.appendChild(span);
+      span.onclick = clicked;
+    });
+    const board = document.getElementById('board');
+    board.appendChild(div);
+  });
+  return { show, array };
+};
+
+
+window.onload = () => {
+  const board = Board();
+  board.show();
+  const player1 = Player('Daniel', 'x');
+  const player2 = Player('Bot', 'o');
+  game = Game(player1, player2);
+  turn = game.p1;
+};
